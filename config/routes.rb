5236a7f3162b89d1d1665960bd5cf069e3013684
hiro.fuji <ec2-user@ip-172-31-42-140.ec2.internal>
2019-05-08 17:06:47 +0000

    to add login function

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,30 @@
 Rails.application.routes.draw do
+  get 'users/new'
+
+  get 'users/create'
+
+  get 'users/destroy'
+
+  get 'user/new'
+
+  get 'user/createdestroy'
+
+  get 'sessions/user'
+
+  get 'sessions/new'
+
+  get 'sessions/create'
+
+  get 'sessions/destroy'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
     root to: 'tasks#index'
+    
+    get 'login', to: 'sessions#new'
+    post 'login', to: 'sessions#create'
+    delete 'logout', to: 'sessions#destroy'
+    get 'signup', to: 'users#new'
     resources :tasks
+    resources :users, only: [:new, :create]
 end
